b5a1f81828592a7fc7ca5c63d08420e668b5d657
13029866356@163.com <liubonan@liubonandeMBP.lan>
2020-05-04 00:11:30 +0800

    add code

--- tfTwo/simpleModel.py
+++ tfTwo/simpleModel.py
@@ -37,3 +37,63 @@ for i in range(100):
     optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
 print(model.variables)
 '''
+
+
+class MNISTLoader():
+    def __init__(self):
+        mnist = tf.keras.datasets.mnist
+        (self.train_data, self.train_label), (self.test_data, self.test_label) = mnist.load_data()
+        # 数据处理部分 标准化 / 将整个数据除了第一个维度 （batch维度） 扩展
+        self.train_data = np.expand_dims(self.train_data.astype(np.float32) / 255.0, axis=-1)
+        self.test_data = np.expand_dims(self.test_data.astype(np.float32) / 255.0, axis=-1)
+        self.train_label = self.train_label.astype(np.int32)
+        self.test_label = self.test_label.astype(np.int32)
+        self.num_train_data, self.num_test_data = self.train_data.shape[0], self.test_data.shape[0]
+    
+    def get_batch(self, batch_size):
+        index = np.random.randint(0, np.shape(self.train_data)[0], batch_size)
+        return self.train_data[index, :], self.train_label[index]
+
+
+class MLP(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.flatten = tf.keras.layers.Flatten()
+        self.dense1 = tf.keras.layers.Dense(units=100, activation=tf.nn.relu)
+        self.dense2 = tf.keras.layers.Dense(units=100)
+
+    def call(self, inputs):
+        x = self.flatten(inputs)
+        x = self.dense1(x)
+        x = self.dense2(x)
+        output = tf.nn.softmax(x)
+        return output
+
+
+num_epochs = 2
+batch_size = 100
+learning_rate = 0.001
+
+model = MLP()
+data_loader = MNISTLoader()
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+
+num_batches = int(data_loader.num_train_data // batch_size*num_epochs)
+for batch_index in range(num_batches):
+    X, y = data_loader.get_batch(batch_size)
+    with tf.GradientTape() as tape:
+        y_pre = model(X)
+        # 交叉熵损失函数
+        loss = tf.keras.losses.sparse_categorical_crossentropy(y_true=y, y_pred=y_pre)
+        loss = tf.reduce_mean(loss)
+        print("batch %d: loss %f" % (batch_index, loss.numpy()))
+    grads = tape.gradient(loss, model.variables)
+    optimizer.apply_gradients(grads_and_vars=zip(grads, model.variables))
+    
+sca = tf.keras.metrics.SparseCategoricalAccuracy()
+num_batches = int(data_loader.num_test_data // batch_size)
+for batch_index in range(num_batches):
+    start_index, end_index = batch_index * batch_size, (batch_index + 1) * batch_size
+    y_pre = model.predict(data_loader.test_data[start_index:end_index])
+    sca.update_state(y_true=data_loader.test_label[start_index:end_index], y_pred=y_pre)
+print("test accuracy: %f" % sca.result())
